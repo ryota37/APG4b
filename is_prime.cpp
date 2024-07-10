ee986e79dc332e534becb6f23c321de9719748b1
ryota37 <ryota.nakahama@access-company.com>
2024-07-10 23:06:50 +0900

    implemented is_prime.cpp

--- is_prime.cpp
+++ is_prime.cpp
@@ -1,17 +1,34 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-
-int is_prime(int n) {
-    // ベースケース
-
-    // 処理内容
+bool is_divisible (int n, int divisor) {
+    if (divisor * divisor > n) {
+        return false;
+    } else if (n % divisor == 0) {
+        return true;
+    } else {
+        return is_divisible(n, divisor + 1);
+    }
 }
 
-
+string is_prime(int n) {
+    // ベースケース
+    if (n == 1) {
+        return "No";
+    } else if (n == 2) {
+        return "Yes";
+    } else {
+    // 再帰ステップ
+        if (is_divisible(n, 2)) {
+            return "No";
+        } else {
+            return "Yes";
+        }
+    }
+}
 
 int main() {
-
+    int n;
+    cin >> n;
+    cout << is_prime(n);
 }
-
-// 引数だけは明らかだけど、ベースケースと処理内容は思いつきすらしない
